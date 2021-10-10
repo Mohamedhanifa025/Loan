6cf33f4d953a79645a41ed3ff2320e952530334c
mohamedhanifa025 <hanifa025@gmail.com>
2021-10-11 00:36:43 +0530

    Notification table code updated

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -1,14 +1,70 @@
 <?php
 
 namespace App\Http\Controllers;
-
+namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Notification;
 
 class NotificationController extends Controller
 {
-    public function index()
-    {
-      return view('notificaton');
-    }
+  public function index()
+  {
+      $notifications = Notification::all();
+    return view('notification.index' , compact('notifications'));
+  }
+  public function create()
+  {
+      abort_unless(\Gate::allows('notification_create'), 403);
+
+      return view('notification.create');
+  }
+
+  public function store(Request $request)
+  {
+
+      $notification = Notification::create($request->all());
+
+      return redirect()->route('notification.index');
+  }
+
+  public function edit(Notification $notification)
+  {
+      abort_unless(\Gate::allows('notification_edit'), 403);
+
+      return view('notification.edit', compact('notification'));
+  }
+
+  public function update(Request $request, Notification $notification)
+  {
+      abort_unless(\Gate::allows('notification_edit'), 403);
+
+      $notification->update($request->all());
+
+      return redirect()->route('notification.index');
+  }
+
+  public function show(Notification $notification)
+  {
+      abort_unless(\Gate::allows('notification_show'), 403);
+
+      return view('notification.show', compact('notification'));
+  }
+
+  public function destroy(Notification $notification)
+  {
+      abort_unless(\Gate::allows('notification_delete'), 403);
+
+      $notification->delete();
+
+      return back();
+  }
+
+  public function massDestroy(Request $request)
+  {
+    Notification::whereIn('id', request('ids'))->delete();
+
+      return response(null, 204);
+  }
 }
