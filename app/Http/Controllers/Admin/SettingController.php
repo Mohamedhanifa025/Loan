3a548ff72ab98c43e03d559578eb5b67d26e43cd
mohamedhanifa025 <hanifa025@gmail.com>
2021-10-14 16:38:29 +0530

    setting table code updated

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -7,8 +7,62 @@ use Illuminate\Http\Request;
 
 class SettingController extends Controller
 {
-    public function index()
-    {
-      return view('setting');
-    }
+  public function index()
+  {
+      $settings = Setting::all();
+    return view('admin.setting.index' , compact('settings'));
+  }
+  public function create()
+  {
+      abort_unless(\Gate::allows('setting_create'), 403);
+
+      return view('admin.setting.create');
+  }
+
+  public function store(Request $request)
+  {
+
+      $setting = Setting::create($request->all());
+
+      return redirect()->route('admin.setting.index');
+  }
+
+  public function edit(Customer $customer)
+  {
+      abort_unless(\Gate::allows('setting_edit'), 403);
+
+      return view('admin.setting.edit', compact('setting'));
+  }
+
+  public function update(Request $request, Setting $setting)
+  {
+      abort_unless(\Gate::allows('customer_edit'), 403);
+
+      $setting->update($request->all());
+
+      return redirect()->route('admin.setting.index');
+  }
+
+  public function show(Setting $setting)
+  {
+      abort_unless(\Gate::allows('setting_show'), 403);
+
+      return view('admin.setting.show', compact('setting'));
+  }
+
+  public function destroy(Setting $setting)
+  {
+      abort_unless(\Gate::allows('setting_delete'), 403);
+
+      $setting->delete();
+
+      return back();
+  }
+
+  public function massDestroy(Request $request)
+  {
+    Setting::whereIn('id', request('ids'))->delete();
+
+      return response(null, 204);
+  }
 }
